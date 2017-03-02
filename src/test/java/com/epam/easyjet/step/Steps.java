ca31f8c806cc4df467913f0c49db2fd9a6740f2b
Yauheni Borbut <Yauheni_Borbut@epam.com>
2017-03-02 13:42:42 +0300

    added tests, reworked main page

--- src/test/java/com/epam/easyjet/step/Steps.java
+++ src/test/java/com/epam/easyjet/step/Steps.java
@@ -1,7 +1,39 @@
 package com.epam.easyjet.step;
 
+import com.epam.easyjet.driver.DriverSingleton;
+import com.epam.easyjet.page.MainPage;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by Yauheni_Borbut on 2/28/2017.
  */
 public class Steps {
+
+    private WebDriver driver;
+    MainPage mainPage;
+
+    public void initDriver() {
+        driver = DriverSingleton.getDriver();
+    }
+
+    public void closeDriver() {
+        driver.close();
+    }
+
+    public void setClientCount(int adultCount, int childCount, int infantCount) {
+        if (null == mainPage) {
+            mainPage = new MainPage(driver);
+        }
+        mainPage.openPage();
+        mainPage.setAdultCount(adultCount);
+        mainPage.setChildCount(childCount);
+        mainPage.setInfantCount(infantCount);
+    }
+
+    public boolean isWarningDisplayed() {
+        if (null == mainPage) {
+            mainPage = new MainPage(driver);
+        }
+        return mainPage.isWarningPresents();
+    }
 }
--- src/test/java/com/epam/easyjet/test/AutomationTest.java
+++ src/test/java/com/epam/easyjet/test/AutomationTest.java
@@ -3,16 +3,23 @@ package com.epam.easyjet.test;
 import com.epam.easyjet.driver.DriverSingleton;
 import com.epam.easyjet.page.FlightsPage;
 import com.epam.easyjet.page.MainPage;
+import com.epam.easyjet.step.Steps;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 /**
  * Created by Yauheni_Borbut on 2/28/2017.
  */
 public class AutomationTest {
-    WebDriver driver;
 
+    WebDriver driver;
+    Steps steps;
+
+    /**
+     * Method should be deleted, when all pages will be ready
+     */
     @Test
     public void test() throws Exception {
         driver = DriverSingleton.getDriver();
@@ -28,12 +35,51 @@ public class AutomationTest {
         mainPage.setChildCount(1);
         Thread.sleep(1000);
         mainPage.submit();
-        driver.getCurrentUrl();
         FlightsPage flightsPage = new FlightsPage(driver);
         flightsPage.openPage();
         flightsPage.clickDeparturePrice();
         flightsPage.clickReturnPrice();
         Thread.sleep(1000);
         flightsPage.submit();
+        System.out.println(driver.getCurrentUrl());
     }
+
+    /*@BeforeTest
+    public void setUp() {
+        steps = new Steps();
+        steps.initDriver();
+    }
+
+    @DataProvider(name = "invalid client count")
+    public Object[][] getInvalidData() throws Exception {
+        return new Object[][] {
+                {41, 0 , 0},
+                {0, 41, 0},
+                {20, 21, 0},
+                {1, 1, 2},
+                {8, 8, 8}
+        };
+    }
+
+    @DataProvider(name = "valid client count")
+    public Object[][] getValidData() throws Exception {
+        return new Object[][] {
+                {40, 0 , 0},
+                {20, 20, 0},
+                {1, 1, 1},
+                {7, 0, 7}
+        };
+    }
+
+    @Test(dataProvider = "invalid client count")
+    public void testInvalidCountOfClients(int adultCount, int childCount, int infantCount) throws Exception {
+        steps.setClientCount(adultCount, childCount, infantCount);
+        Assert.assertTrue(steps.isWarningDisplayed());
+    }
+
+    @Test(dataProvider = "valid client count")
+    public void testValidCountOfClients(int adultCount, int childCount, int infantCount) throws Exception {
+        steps.setClientCount(adultCount, childCount, infantCount);
+        Assert.assertTrue(!steps.isWarningDisplayed());
+    }*/
 }
