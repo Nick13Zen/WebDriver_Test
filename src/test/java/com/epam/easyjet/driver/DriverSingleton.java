8fd91583963e51b40e1b50b0a84839838d778fae
Yauheni Borbut <Yauheni_Borbut@epam.com>
2017-03-15 15:53:17 +0300

    reworked tests

--- src/test/java/com/epam/easyjet/driver/DriverSingleton.java
+++ src/test/java/com/epam/easyjet/driver/DriverSingleton.java
@@ -2,6 +2,9 @@ package com.epam.easyjet.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,14 +14,19 @@ public class DriverSingleton {
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
     private static final String GECKODRIVER_GECKODRIVER_EXE_PATH = ".\\geckodriver\\geckodriver.exe";
 
+    private static final String NORMAL_PARAMETER = "normal";
+
     private DriverSingleton() {
     }
 
     public static WebDriver getDriver() {
         if (null == driver) {
             System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_GECKODRIVER_EXE_PATH);
-            driver = new FirefoxDriver();
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+            DesiredCapabilities firefoxCap = DesiredCapabilities.firefox();
+            firefoxCap.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, NORMAL_PARAMETER);
+
+            driver = new FirefoxDriver(firefoxCap);
+            driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.manage().window().maximize();
         }
